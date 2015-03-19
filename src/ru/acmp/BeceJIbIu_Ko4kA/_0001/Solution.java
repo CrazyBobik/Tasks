621224ef5468dd43d881ab379d881a20da3dd590
CrazyBobik <akrasnyanchin@mail.ru>
2015-03-19 17:16:50 +0200

    Initial commit

---
+++ src/ru/acmp/BeceJIbIu_Ko4kA/_0001/Solution.java
@@ -0,0 +1,35 @@
+package ru.acmp.BeceJIbIu_Ko4kA._0001;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Требуется сложить два целых числа А и В.
+ *
+ *  Входные данные:
+ *  В единственной строке входного файла INPUT.TXT записано два натуральных числа через пробел,
+ *  не превышающих 10^9.
+ *
+ *  Выходные данные:
+ *   В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел А и В.
+ */
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new FileInputStream("d:\\input.txt"));
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+
+        PrintWriter out = new PrintWriter(new FileOutputStream("d:\\output.txt"));
+        out.println(a + b);
+        out.close();
+    }
+}
+
+/**
+ * Решено!=)
+ */
---
+++ src/ru/acmp/BeceJIbIu_Ko4kA/_0002/Solution.java
@@ -0,0 +1,33 @@
+package ru.acmp.BeceJIbIu_Ko4kA._0002;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ *  Требуется посчитать сумму целых чисел от 1 до N.
+ *  Входные данные
+ *   В единственной строке входного файла INPUT.TXT записано единственное целое число N, не превышающее
+ *   по абсолютной величине 10^4.
+ *
+ *   Выходные данные
+ *   В единственную строку выходного файла OUTPUT.TXT нужно вывести одно целое число — сумму чисел от 1 до N.
+ */
+
+public class Solution {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new FileInputStream("d:\\input.txt"));
+        int a = in.nextInt();
+        in.close();
+
+        PrintWriter out = new PrintWriter(new FileOutputStream("d:\\output.txt"));
+        int sum = 0;
+        for (int i = 1; i <= a; i++) {
+            sum += i;
+        }
+        out.println(sum);
+        out.close();
+    }
+}
